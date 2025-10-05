d24c92dccd6c054047216f60005e20fa3e15242f
MTofayelAhmed <tofayelahmed324@gmail.com>
2025-10-06 01:20:26 +0200

    done with basic data type

---
+++ module/src/1.4.ts
@@ -0,0 +1,19 @@
+// primitive and non-primitive type 
+// basic data type
+
+const X: number = 5;
+const Y: string = "Beautiful"
+const Z: boolean = true 
+const value : null = null 
+const personName: undefined = undefined 
+
+let age : number
+
+age = 24 
+
+
+// Non Primitive Data Type  
+
+const numberArray : number[]=[1, 2, 3]
+
+const stringArray : string[]= ["65", "Pet", "Cat"]
